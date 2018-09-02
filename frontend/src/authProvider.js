e4e43cc7bc84cdfc16049cbfa6a6c6d7ae0cd944
Anuar Kilgore <jaicof@gmail.com>
2018-09-02 01:09:27 -0600

    added login

--- frontend/src/authProvider.js
+++ frontend/src/authProvider.js
@@ -2,7 +2,20 @@ import { AUTH_LOGIN, AUTH_LOGOUT, AUTH_ERROR, AUTH_CHECK } from 'react-admin';
 
 export default (type, params) => {
     if (type === AUTH_LOGIN) {
-        
+        let { username, password } = params;
+        console.log(process.env.REACT_APP_RESTPOS_HOST+"/auth");
+        const request = new Request(process.env.REACT_APP_RESTPOS_HOST+"/auth", {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            // headers: new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' })
+            headers: new Headers({ 'Content-Type': 'application/json' })
+        });
+
+        return fetch(request)
+        .then(res => res.ok ? res.json() : Promise.reject())
+        .then(({key}) => {
+            localStorage.setItem("authKey", key);
+        });
     }
     if (type === AUTH_LOGOUT) {
         localStorage.removeItem('authKey');
@@ -16,6 +29,7 @@ export default (type, params) => {
         return Promise.resolve();
     }
     if (type === AUTH_CHECK) {
-        return localStorage.getItem('token') ? Promise.resolve() : Promise.reject({redirectTo: '/no-access'});
+        return localStorage.getItem('authKey') ? Promise.resolve() : Promise.reject();
+        // return localStorage.getItem('authKey') ? Promise.resolve() : Promise.reject({redirectTo: '/no-access'});
     }
 }
